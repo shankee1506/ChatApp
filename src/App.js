dc6f6a854453cb0d9a61c39420d9de978c2f3d9c
shankee1506 <pshankeerthan@gmail..com>
2022-03-07 22:30:17 +0530

    create components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import {ChatEngine} from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <ChatEngine 
+        height="100vh"
+        projectID="9d7dbcbf-9959-4490-8386-dcbb71905b40"
+        userName="Tony12"
+        userSecret="Tony1234"
+        renderChatFeed={(chatAppProps)=> <ChatFeed  {...chatAppProps}/>}
+    />
+  );
 }
 
 export default App
